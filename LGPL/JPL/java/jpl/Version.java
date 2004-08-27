be83a4a3090845e609eef8e2e82450c09b49041a
vsc <vsc@b08c6af1-5177-4d33-ba66-4b1c6b8b522a>
2004-08-27 20:27:56 +0000

    jpl stuff
    git-svn-id: https://yap.svn.sf.net/svnroot/yap/trunk@1120 b08c6af1-5177-4d33-ba66-4b1c6b8b522a

---
+++ LGPL/JPL/java/jpl/Version.java
@@ -0,0 +1,9 @@
+// $Id: Version.java,v 1.1 2004-08-27 20:27:56 vsc Exp $
+package jpl;
+class Version
+{
+	public final int    major            = 3;
+	public final int    minor            = 0;
+	public final int    patch            = 3;
+	public final String status           = "alpha";
+}
