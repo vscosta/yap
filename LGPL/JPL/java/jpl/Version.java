7b993ca987584d6c822a23f3dccd9f55d310ca1c
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2009-05-02 14:11:10 -0500

    this should be in packages, not in LGPL!

--- LGPL/JPL/java/jpl/Version.java
+++
@@ -1,9 +0,0 @@
-// $Id: Version.java,v 1.2 2007-09-27 15:25:32 vsc Exp $
-package jpl;
-
-class Version {
-	public final int    major            = 3;
-	public final int	minor	= 1;
-	public final int    patch            = 3;
-	public final String status           = "alpha";
-}
