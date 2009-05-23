b5e5a671a82f12a6a433663e9edadf7a37abeee4
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2009-05-23 15:51:50 -0500

    restore stack on JumpExec

--- C/exec.c
+++ C/exec.c
@@ -1541,7 +1541,7 @@ JumpToEnv(Term t) {
   B->cp_env = (CELL *)env[E_E];
   B->cp_ap = NEXTOP(PredHandleThrow->CodeOfPred,l);
   /* can recover Heap thanks to copy term :-( */
-  /* B->cp_h = H; */
+  B->cp_h = H;
   /* I could backtrack here, but it is easier to leave the unwinding
      to the emulator */
   B->cp_a3 = t;
