f10dde5b906e9924ba77ef7f4f635eab78977c76
Costa Vitor <vitor@quasar-20.(none)>
2009-06-02 16:36:00 -0500

    make sure garbage collector goes to FAILCODE, not do a hard reset.

--- C/heapgc.c
+++ C/heapgc.c
@@ -3974,17 +3974,8 @@ LeaveGCMode()
     Yap_PrologMode &= ~GCMode;
   if (Yap_PrologMode & AbortMode) {
     Yap_PrologMode &= ~AbortMode;
-      Yap_Error(PURE_ABORT, TermNil, "");
-      /* in case someone mangles the P register */
-      save_machine_regs();
-#if  _MSC_VER || defined(__MINGW32__)
-      /* don't even think about trying this */
-#else
-#if PUSH_REGS
-      restore_absmi_regs(&Yap_standard_regs);
-#endif
-      siglongjmp (Yap_RestartEnv, 1);
-#endif
+    Yap_Error(PURE_ABORT, TermNil, "");
+    P = FAILCODE;
   }
 }
 
