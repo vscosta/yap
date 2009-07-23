22ac3ee80bc70b4804a42b2b3e4e952eff2bb334
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2009-07-23 14:31:04 -0500

    Use >< for xor (Paulo Mour, Iso meeting)

--- C/arith2.c
+++ C/arith2.c
@@ -1092,6 +1092,7 @@ static InitBinEntry InitBinTab[] = {
   {"/\\", op_and},
   {"\\/", op_or},
   {"#", op_xor},
+  {"><", op_xor},
   {"atan2", op_atan2},
   /* C-Prolog exponentiation */
   {"^", op_power},
