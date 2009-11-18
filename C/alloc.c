9cbdf8f57e4731f5a7723dc78b5e0d98de214bde
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2009-11-18 23:36:07 +0000

    tiny cleanups.

--- C/alloc.c
+++ C/alloc.c
@@ -1412,7 +1412,7 @@ Yap_InitMemory(UInt Trail, UInt Heap, UInt Stack)
 #if RANDOMIZE_START_ADDRESS
   srand(time(NULL));
   UInt x = (rand()% 100)*YAP_ALLOC_SIZE ;
-  fprintf(stderr,"x=%ulx\n", (unsigned long int)x);
+  fprintf(stderr,"x=%lx\n", (unsigned long int)x);
   pm += x;
 #endif
   addr = InitWorkSpace(pm);
