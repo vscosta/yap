56d9666197d1a7b914d6d2299ad902c7993068dd
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2009-11-23 10:55:10 +0000

    port of SWI-Prolog's plunit.

---
+++ packages/plunit/examples/read.pl
@@ -0,0 +1,22 @@
+
+:- use_module(library(plunit)).
+
+:- begin_tests(read).
+
+:- use_module(library(system)).
+
+:- use_module(library(readutil)).
+
+create_file(Tmp) :-
+	tmp_file(plunit, Tmp),
+	open(Tmp, write, Out),
+	write(Out, 'hello(World).\n'),
+	close(Out).
+
+test(read, [ setup(create_file(Tmp)),
+	     cleanup(delete_file(Tmp))
+	   ]) :-
+	read_file_to_terms(Tmp, Terms, []),
+	Terms =@= [hello(_)].
+
+%:- end_tests(read).
