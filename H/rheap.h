25b5464fe85463a958b0027ac5f0fc9c1dd36ebe
U-Xato7\Vsc <Vsc@Xato7.(none)>
2009-11-23 16:02:05 +0000

    handle correctly next element after operator property.

--- H/rheap.h
+++ H/rheap.h
@@ -1523,6 +1523,10 @@ RestoreEntries(PropEntry *pp, int int_key)
     case OpProperty:
       {
 	OpEntry *opp = (OpEntry *)pp;
+	if (opp->NextOfPE) {
+	  opp->NextOfPE =
+	    PropAdjust(opp->NextOfPE);
+	}
 	if (opp->OpModule) {
 	 opp->OpModule = AtomTermAdjust(opp->OpModule);
 	}
@@ -1569,13 +1573,14 @@ RestoreAtom(AtomEntry *at)
 {
   AtomEntry *nat;
 
-#ifdef DEBUG_RESTORE2			/* useful during debug */
+  /* this should be done before testing for wide atoms */
+  at->PropsOfAE = PropAdjust(at->PropsOfAE);
+#if DEBUG_RESTORE2			/* useful during debug */
   if (IsWideAtom(AbsAtom(at)))
     fprintf(errout, "Restoring %S\n", at->WStrOfAE);
   else
     fprintf(errout, "Restoring %s\n", at->StrOfAE);
 #endif
-  at->PropsOfAE = PropAdjust(at->PropsOfAE);
   RestoreEntries(RepProp(at->PropsOfAE), FALSE);
   /* cannot use AtomAdjust without breaking agc */
   nat = RepAtom(at->NextOfAE);
