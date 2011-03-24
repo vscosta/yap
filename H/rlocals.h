0352410666da868b623e34231f5a9313d3fe5348
Vítor Santos Costa <vsc@xato.(none)>
2011-03-24 10:25:02 +0000

    support separate file for LOCALS

---
+++ H/rlocals.h
@@ -0,0 +1,146 @@
+
+  /* This file, rlocals.h, was generated automatically by "yap -L misc/buildheap"
+     please do not update, update misc/LOCALS instead */
+
+
+static void RestoreWorker(int wid USES_REGS) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  FOREIGN_WL(wid)->global_arena = TermToGlobalOrAtomAdjust(FOREIGN_WL(wid)->global_arena);
+
+
+
+
+
+
+
+
+
+
+  RestoreBallTerm(wid);
+
+
+
+
+
+
+
+#ifdef  COROUTINING
+  FOREIGN_WL(wid)->woken_goals = TermToGlobalAdjust(FOREIGN_WL(wid)->woken_goals);
+  FOREIGN_WL(wid)->atts_mutable_list = TermToGlobalAdjust(FOREIGN_WL(wid)->atts_mutable_list);
+#endif
+
+  FOREIGN_WL(wid)->gc_generation = TermToGlobalAdjust(FOREIGN_WL(wid)->gc_generation);
+  FOREIGN_WL(wid)->gc_phase = TermToGlobalAdjust(FOREIGN_WL(wid)->gc_phase);
+
+
+
+
+
+
+#if LOW_LEVEL_TRACER
+
+#endif
+
+#if defined(YAPOR) || defined(THREADS)
+  REINIT_LOCK(FOREIGN_WL(wid)->signal_lock);
+
+
+
+
+#if DEBUG && COROUTINING
+
+#endif
+
+
+
+
+#if GC_NO_TAGS
+
+#endif
+#if defined(TABLING) || defined(SBA)
+
+
+
+#else
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+#endif /* defined(YAPOR) || defined(THREADS) */
+
+  FOREIGN_WL(wid)->dynamic_arrays = PtoArrayEAdjust(FOREIGN_WL(wid)->dynamic_arrays);
+  FOREIGN_WL(wid)->static_arrays = PtoArraySAdjust(FOREIGN_WL(wid)->static_arrays);
+  FOREIGN_WL(wid)->global_variables = PtoGlobalEAdjust(FOREIGN_WL(wid)->global_variables);
+
+
+
+
+
+
+
+
+
+
+
+#if (defined(YAPOR) || defined(TABLING)) && defined(THREADS)
+
+#endif
+#ifdef THREADS
+
+#define FOREIGN_ThreadHandle(wid)  (Yap_WLocal[(wid)]->thread_handle)		       						
+#define MY_ThreadHandle	       (Yap_WLocal[worker_id]->thread_handle)
+#endif
+
+}
