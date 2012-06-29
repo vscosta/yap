e938c1774890aca8a1ce657b2df9d476a3faeb32
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2012-06-29 22:24:12 +0100

    fix clang compilation (Denys Duchier).

--- packages/swi-minisat2/C/SolverTypes.h
+++ packages/swi-minisat2/C/SolverTypes.h
@@ -119,11 +119,7 @@ public:
 
     // -- use this function instead:
     template<class V>
-    friend Clause* Clause_new(const V& ps, bool learnt = false) {
-        assert(sizeof(Lit)      == sizeof(uint32_t));
-        assert(sizeof(float)    == sizeof(uint32_t));
-        void* mem = malloc(sizeof(Clause) + sizeof(uint32_t)*(ps.size()));
-        return new (mem) Clause(ps, learnt); }
+    friend Clause* Clause_new(const V& ps, bool learnt = false);
 
     int          size        ()      const   { return size_etc >> 3; }
     void         shrink      (int i)         { assert(i <= size()); size_etc = (((size_etc >> 3) - i) << 3) | (size_etc & 7); }
@@ -147,6 +143,12 @@ public:
 };
 
 
+template<class V>
+Clause* Clause_new(const V& ps, bool learnt) {
+        assert(sizeof(Lit)      == sizeof(uint32_t));
+        assert(sizeof(float)    == sizeof(uint32_t));
+        void* mem = malloc(sizeof(Clause) + sizeof(uint32_t)*(ps.size()));
+        return new (mem) Clause(ps, learnt); }
 /*_________________________________________________________________________________________________
 |
 |  subsumes : (other : const Clause&)  ->  Lit
