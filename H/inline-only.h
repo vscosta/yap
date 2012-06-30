a34b45bcc032dc8ea4655cf8f838f0f46beb0939
Denys Duchier <denys.duchier@univ-orleans.fr>
2012-06-30 17:51:47 +0200

    distinguish gcc and clang for INLINE_ONLY

--- H/inline-only.h
+++ H/inline-only.h
@@ -1,10 +1,14 @@
 #ifndef _YAP_INLINE_ONLY_H_
 #define _YAP_INLINE_ONLY_H_
 
-#ifdef __GNUC__
+#ifdef __clang__
 #define INLINE_ONLY __attribute__((gnu_inline,always_inline))
 #else
+#ifdef __GNUC__
+#define INLINE_ONLY __attribute__((always_inline))
+#else
 #define INLINE_ONLY
+#endif
 #endif
 
 #endif
