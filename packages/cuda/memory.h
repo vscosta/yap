ffd9bb7a19e3e726d0489d5fda245c11d57e1365
Vítor Santos Costa <vsc@cracs-gpu.dcc.fc.up.pt>
2013-10-09 13:33:10 +0100

    missing headers

---
+++ packages/cuda/memory.h
@@ -0,0 +1,26 @@
+#ifndef _MEMORY_H_
+#define _MEMORY_H_
+
+//#include <thrust/device_vector.h>
+#include <list>
+#include <vector>
+#include "lista.h"
+
+using namespace std;
+//using namespace thrust;
+
+void calcular_mem(int);
+void liberar(int*, int);
+void limpiar();
+void limpiartodo(int*, int*);
+int cargar(int, int, int, int, int*, int**, int);
+int cargafinal(int, int, int**);
+void reservar(int**, int);
+void registrar(int, int, int*, int, int, int);
+bool generadas(int, int, int, int);
+void mostrar_memoria(void);
+void mostrar_memcpu(void);
+void clear_memory(void);
+void resultados(vector<rulenode>::iterator, vector<rulenode>::iterator);
+
+#endif
