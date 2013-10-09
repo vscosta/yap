a165d3e4c2ded8dc73cca45cff7dedd2cf8c4763
Vítor Santos Costa <vsc@cracs-gpu.dcc.fc.up.pt>
2013-10-09 14:41:53 +0100

    missing heder

---
+++ packages/cuda/lista.h
@@ -0,0 +1,41 @@
+#ifndef _LISTA_H_
+#define _LISTA_H_
+
+typedef struct Node{
+	int name;
+	int *dev_address;
+	int rows;
+	int size;
+	int iteration;
+	int isrule;
+}memnode;
+
+typedef struct auxiliar{
+	int name;
+	int num_rows;
+	int num_columns;
+	int *address_host_table;
+	int *rule_names;
+	int *referencias;
+	int **select;
+	int *numsel;
+	int **project;
+	int2 *projpos;
+	int **selfjoin;
+	int *numselfj;
+	int **wherejoin;
+	int *numjoin;
+	int3 num_bpreds;
+	int *builtin;
+	int gen_act;
+	int gen_ant;
+}rulenode;
+
+typedef struct completed{
+	int name;
+	int numrules;
+	int reduce;
+	int reset;
+}compnode;
+
+#endif
