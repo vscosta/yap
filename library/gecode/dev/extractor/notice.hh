1ede2112c6e99cf7456156c14b82c3f71b233e64
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2013-11-03 23:55:09 +0000

    distributed configure

--- library/gecode/dev/extractor/notice.hh
+++
@@ -1,3 +0,0 @@
-// This file was automatically extracted from Gecode source files.
-// It is subject to the same Copyright as the source files from which
-// it is derived, and is distributed under the same Licensing conditions.
