3087d51b18224305e649f7b7f68a00e5f6041935
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2014-06-04 22:07:37 +0100

    more swig upddates.

--- packages/swig/android/SwigSimple.java
+++ packages/swig/android/SwigSimple.java
@@ -6,50 +6,84 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.ScrollView;
 import android.text.method.ScrollingMovementMethod;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import  android.content.pm.PackageManager.NameNotFoundException; 
+import  android.util.Log;
+import android.content.res.AssetManager;
 
 public class SwigSimple extends Activity
 {
     TextView outputText = null;
     ScrollView scroller = null;
+	YAPEngine eng = null;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
+    	
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+       	String s = "ugh";
+                setContentView(R.layout.main);
 
+        try {
+        	PackageManager m = getPackageManager();
+        	s = getPackageName();
+        	PackageInfo p = m.getPackageInfo(s, 0);
+        	s = p.applicationInfo.dataDir;
+            mgr = getResources().getAssets();
+            load(mgr);
+      } catch(NameNotFoundException e) { 
+            Log.e(TAG, "Couldn't find package  information in PackageManager", e); 
+        } 
+        
         outputText = (TextView)findViewById(R.id.OutputText);
-        outputText.setText("Press 'Run' to start...\n");
+        outputText.setText("Application " + s + "\nPress 'Run' to start...\n");
         outputText.setMovementMethod(new ScrollingMovementMethod());
-
+        YAPParams p = new YAPParams();
+        eng = new YAPEngine(  p );      
+   
         scroller = (ScrollView)findViewById(R.id.Scroller);
     }
 
     public void onRunButtonClick(View view)
     {
       outputText.append("Started...\n");
-      nativeCall();
+      if (BuildConfig.DEBUG) {
+    	  Log.i(TAG, "onRunButtonClick called");
+    	} 
       outputText.append("Finished!\n");
 
       // Ensure scroll to end of text
       scroller.post(new Runnable() {
         public void run() {
           scroller.fullScroll(ScrollView.FOCUS_DOWN);
+          String s = "['/assets/share/Yap/lists'].\n";
+          outputText.append(s);
+          YAPQuery q = eng.query(s);
+          q.next();
+          String s = "member(X, [1,2,3]).\n";
+          outputText.append(s);
+          YAPQuery q = eng.query(s);
+          q.next();
+          String sn = 
         }
       });
     }
 
-    /** Calls into C/C++ code */
-    public void nativeCall()
-    {
-	//	YAPParams p = new YAPParams();
-	//YAPEngine t = new YAPEngine(  p );      // TODO
-    }
 
     /** static constructor */
     static {
+        System.loadLibrary("android");
+        System.loadLibrary("log");
         System.loadLibrary("example");
     }
+    
+    private static native void load(AssetManager mgr);
+
+    private AssetManager mgr;
+
+    private static final String TAG = "SwigSimple";
 
 }
