db2d6a0ebd0774785035e2c86e09459f75207822
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2014-06-30 14:34:58 +0100

    Start handling Prolog exceptions

---
+++ CXX/yapie.hh
@@ -0,0 +1,16 @@
+
+class YAPEngine;
+class YAPAtom;
+class YAPFunctor;
+class YAPApplTerm;
+class YAPPairTerm;
+class YAPQuery;
+class YAPPredicate;
+
+
+class YAPError {
+public:
+  static const int SYNTAX_ERROR = 0x10000;
+};
+
+
