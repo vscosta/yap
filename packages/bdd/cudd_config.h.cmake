4eaf848e87e925f29336226410467c9ce523deb1
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2015-07-06 14:54:23 +0100

    missing

---
+++ packages/bdd/cudd_config.h.cmake
@@ -0,0 +1,32 @@
+// cmake template file
+
+/* Define to 1 if you have the <cuddInt.h> header file. */
+#ifndef HAVE_CUDDINT_H
+#cmakedefine HAVE_CUDDINT_H ${HAVE_CUDDINT_H}
+#endif
+
+/* Define to 1 if you have the <cudd/cuddInt.h> header file. */
+#ifndef HAVE_CUDD_CUDDINT_H
+#cmakedefine HAVE_CUDD_CUDDINT_H ${HAVE_CUDD_CUDDINT_H}
+#endif
+
+/* Define to 1 if you have the <cudd/cudd.h> header file. */
+#ifndef HAVE_CUDD_CUDD_H
+#cmakedefine HAVE_CUDD_CUDD_H ${HAVE_CUDD_CUDD_H}
+#endif
+
+/* Define to 1 if you have the <cudd.h> header file. */
+#ifndef HAVE_CUDD_H
+#cmakedefine HAVE_CUDD_H ${HAVE_CUDD_H}
+#endif
+
+/* Define to 1 if you have the <cudd/util.h> header file. */
+#ifndef HAVE_CUDD_UTIL_H
+#cmakedefine HAVE_CUDD_UTIL_H ${HAVE_CUDD_UTIL_H}
+#endif
+
+/* Define to 1 if you have the <util.h> header file. */
+#ifndef HAVE_UTIL_H
+#cmakedefine HAVE_UTIL_H ${HAVE_UTIL_H}
+#endif
+
