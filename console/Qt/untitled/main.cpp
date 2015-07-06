9a638d1312def3148e82b3a188ff478d27f1803b
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2015-07-06 11:55:02 +0100

    experiment

--- console/Qt/untitled/main.cpp
+++ console/Qt/untitled/main.cpp
@@ -1,11 +1,40 @@
-#include "mainwindow.h"
-#include <QApplication>
+// main.cpp
+#include <QtCore>
+
+class Task : public QObject
+{
+    Q_OBJECT
+public:
+    Task(QObject *parent = 0) : QObject(parent) {}
+
+public slots:
+    void run()
+    {
+        // Do processing here
+
+        emit finished();
+    }
+
+signals:
+    void finished();
+};
+
+#include "main.moc"
 
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
+    QCoreApplication a(argc, argv);
 
-  return a.exec();
+    // Task parented to the application so that it
+    // will be deleted by the application.
+    Task *task = new Task(&a);
+
+    // This will cause the application to exit when
+    // the task signals finished.
+    QObject::connect(task, SIGNAL(finished()), &a, SLOT(quit()));
+
+    // This will run the task from the application event loop.
+    QTimer::singleShot(0, task, SLOT(run()));
+
+    return a.exec();
 }
