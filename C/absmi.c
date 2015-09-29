8208a181d3802613d210a099d37acc16c8aed649
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2015-09-29 23:01:19 +0100

    thread support

--- C/absmi.c
+++ C/absmi.c
@@ -679,7 +679,7 @@ check_alarm_fail_int(int CONT USES_REGS)
 }
 
 static int
-stack_overflow( PredEntry *pe, CELL *env, yamop *cp USES_REGS, arity_t nargs )
+stack_overflow( PredEntry *pe, CELL *env, yamop *cp, arity_t nargs  USES_REGS)
 {
   if ((Int)(Unsigned(YREG) - Unsigned(HR)) < StackGap( PASS_REGS1 ) ||
       Yap_get_signal( YAP_STOVF_SIGNAL )) {
