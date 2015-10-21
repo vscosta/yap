352ea085b79085fce4b00ae5362af5c2c7be401b
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2015-10-22 00:47:47 +0100

    configuration issues

--- packages/bdd/cudd_config.h
+++ packages/bdd/cudd_config.h
@@ -2,7 +2,7 @@
 
 /* Define to 1 if you have the <cuddInt.h> header file. */
 #ifndef HAVE_CUDDINT_H
-/* #undef HAVE_CUDDINT_H */
+#define HAVE_CUDDINT_H 
 #endif
 
 /* Define to 1 if you have the <cudd/cuddInt.h> header file. */
@@ -17,7 +17,7 @@
 
 /* Define to 1 if you have the <cudd.h> header file. */
 #ifndef HAVE_CUDD_H
-/* #undef HAVE_CUDD_H */
+#define HAVE_CUDD_H 
 #endif
 
 /* Define to 1 if you have the <cudd/util.h> header file. */
