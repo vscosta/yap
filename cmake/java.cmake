54b26d7f234257e8e6a72bc09356ac8c4bc239bd
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2016-04-05 07:49:17 +0100

    unify java
    Former-commit-id: 867c9ea47c98cfcc51bbc66a6e76b323a201bb11

---
+++ cmake/java.cmake
@@ -0,0 +1,51 @@
+#detect java setup, as it is shared between different installations.
+
+find_package(Java COMPONENTS Runtime Development)
+# find_package(Java COMPONENTS Development)
+# find_package(Java COMPONENTS Runtime)
+#find_package(JavaLibs)
+macro_log_feature (Java_Development_FOUND "Java"
+    "Use Java System"
+    "http://www.java.org" FALSE)
+
+if (Java_Development_FOUND)
+
+  find_package(JNI)
+
+  if (JNI_FOUND)
+
+  include(UseJava)
+
+  #
+  #   Java_JAVA_EXECUTABLE      = the full path to the Java runtime
+  #   Java_JAVAC_EXECUTABLE     = the full path to the Java compiler
+  #   Java_JAVAH_EXECUTABLE     = the full path to the Java header generator
+  #   Java_JAVADOC_EXECUTABLE   = the full path to the Java documention generator
+  #   Java_IDLJ_EXECUTABLE      = the full path to the Java idl compiler
+  #   Java_JAR_EXECUTABLE       = the full path to the Java archiver
+  #   Java_JARSIGNER_EXECUTABLE = the full path to the Java jar signer
+  #   Java_VERSION_STRING       = Version of java found, eg. 1.6.0_12
+  #   Java_VERSION_MAJOR        = The major version of the package found.
+  #   Java_VERSION_MINOR        = The minor version of the package found.
+  #   Java_VERSION_PATCH        = The patch version of the package found.
+  #   Java_VERSION_TWEAK        = The tweak version of the package found (after '_')
+  #   Java_VERSION              = This is set to: $major.$minor.$patch(.$tweak)
+  #
+  # The Java_ADDITIONAL_VERSIONS variable can be used to specify a list
+  # of version numbers that should be taken into account when searching
+  # for Java.  You need to set this variable before calling
+  # find_package(JavaLibs).
+  #
+  #macro_optional_find_package(JNI ON)
+  #   JNI_INCLUDE_DIRS      = the include dirs to use
+  #   JNI_LIBRARIES         = the libraries to use
+  #   JNI_FOUND             = TRUE if JNI headers and libraries were found.
+  #   JAVA_AWT_LIBRARY      = the path to the jawt library
+  #   JAVA_JVM_LIBRARY      = the path to the jvm library
+  #   JAVA_INCLUDE_PATH     = the include path to jni.h
+  #   JAVA_INCLUDE_PATH2    = the include path to jni_md.h
+  #   JAVA_AWT_INCLUDE_PATH = the include path to jawt.h
+endif (JNI_FOUND)
+
+
+endif (Java_Development_FOUND)
