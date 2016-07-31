ed21826c2aa16cffc4404ea5f69825847982ebc9
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2016-07-31 10:29:36 -0500

    plit cidd testing
    Former-commit-id: 19ec491d223ad3fbc791bb271cdc12c5377c5bbf

--- cmake/cudd.cmake
+++ cmake/cudd.cmake
@@ -1,14 +1,21 @@
+
+ 
+  option (WITH_CUDD "BDD CUDD package" ON)
+if (WITH_CUDD)
 #detect cudd setup, as it is shared between different installations.
 
 find_package(CUDD)
-#  CUDD_FOUND       - system has CUDD 
+#  CUDD_FOUND       - system has CUDD
 #  CUDD_LIBRARIES   - Link these to use CUDD
 #  CUDD_INCLUDE_DIR - Include directory for using CUDD
 #
+if (CUDD_FOUND)
+    set( CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${CUDD_INCLUDE_DIR} )
 
-macro_log_feature (CUDD_FOUND "CUDD"
-  "Use CUDD Library"
-  "http://vlsi.colorado.edu/~fabio/CUDD/" FALSE)
+check_include_files( cudd.h HAVE_CUDD_H )
+check_include_files( "stdio.h;cudd/cudd.h" HAVE_CUDD_CUDD_H )
+check_include_files( cuddInt.h HAVE_CUDDINT_H )
+check_include_files( "stdio.h;cudd/cudd.h;cudd/cuddInt.h" HAVE_CUDD_CUDDINT_H )
+endif (CUDD_FOUND)
 
-
-
+endif(WITH_CUDD)
