1af2451db035cfad3b00eab15d068d3adc2bf851
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2016-08-04 16:42:57 -0500

    people who don't like cmake
    Former-commit-id: fd23360c63d385bf52eefe2a9dd6cec5c642b4d8

---
+++ .configure-custom.sh
@@ -0,0 +1,2 @@
+ENABLE_VARS="jit|no|WITH_JIT clpbn|yes|WITH_CLPBN cplint|yes|WITH_CPLINT horus|yes|WITH_HORUS docs|no|WITH_DOCS problog|yes|WITH_PROBLOG"
+WITH_VARS="swig|yes|WITH_SWIG gecode|yes|WITH_GECODE R|yes|WITH_REAL cudd|yes|WITH_BDD python|yes|WITH_PYTHON "
