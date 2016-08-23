298d3609dad64ad0c59c36f88590ffa15e858579
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2016-08-23 11:15:07 -0500

    cmake
    Former-commit-id: 1bea4230e48462c19b7a2ce7787889de0b0fd5a2

--- cmake/cudd.cmake
+++ cmake/cudd.cmake
@@ -1,6 +1,7 @@
 
  
-  option (WITH_CUDD "BDD CUDD package" ON)
+option (WITH_CUDD "BDD CUDD package" ON)
+
 if (WITH_CUDD)
 #detect cudd setup, as it is shared between different installations.
 
@@ -9,7 +10,12 @@ find_package(CUDD)
 #  CUDD_LIBRARIES   - Link these to use CUDD
 #  CUDD_INCLUDE_DIR - Include directory for using CUDD
 #
+macro_log_feature (CUDD_FOUND "CUDD"
+    "Use CUDD BDD library"
+    "http://vlsi.colorado.edu/~fabio/CUDD/" FALSE)
+
 if (CUDD_FOUND)
+
     set( CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${CUDD_INCLUDE_DIR} )
 
 check_include_files( cudd.h HAVE_CUDD_H )
