7894c4d88d4c30a950d699bae7e38c666f8394de
Vitor Santos Costa <vsc@dcc.fc.up.pt>
2016-12-03 10:35:39 -0600

    improve configure
    Former-commit-id: 490a07ade2c3c192cced40d22827f3716749cf0d

--- .configure-custom.sh
+++ .configure-custom.sh
@@ -1,2 +1,20 @@
-ENABLE_VARS="jit|no|WITH_JIT clpbn|yes|WITH_CLPBN cplint|yes|WITH_CPLINT horus|yes|WITH_HORUS docs|no|WITH_DOCS problog|yes|WITH_PROBLOG"
-WITH_VARS="swig|yes|WITH_SWIG gecode|yes|WITH_GECODE R|yes|WITH_REAL cudd|yes|WITH_BDD python|yes|WITH_PYTHON "
+ENABLE_VARS="clpbn|yes|WITH_CLPBN \
+  cplint|yes|WITH_CPLINT \
+  horus|yes|WITH_HORUS \
+  clpr|yes|WITH_CLPR \
+  problog|yes|WITH_PROBLOG
+   jit|no|WITH_JIT \
+   chr|no|WITH_CHR \
+   threads|no|WITH_THREADS"
+WITH_VARS="swig|yes|WITH_SWIG \
+   mpi|yes|WITH_MPI \
+   gecode|yes|WITH_GECODE \
+   docs|yes|WITH_DOCS \
+   r|yes|WITH_REAL \
+   cudd|yes|WITH_CUDD \
+   xml2|yes|WITH_XML2 \
+   raptor|yes|WITH_RAPTOR \
+   python|yes|WITH_PYTHON \
+   openssl|yes|WITH_OPENSSL\
+   readline|yes|WITH_READLINE \
+   gmp|yes|WITH_GMP " 
