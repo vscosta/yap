a74e2dbe17f837327033674713fd3dfdc61dc9c1
Vítor Santos Costa <vsc@dcc.fc.up.pt>
2019-06-14 16:47:40 +0100

    global
    Former-commit-id: 908207fd097975a0befe92256914de3fa43e3a3e

--- C/globals.c
+++ C/globals.c
@@ -1661,8 +1661,8 @@ static Int p_nb_queue_enqueue(USES_REGS1) {
   } else {
     min_size = 0L;
   }
-  Term entry = MkPairTerm(Deref(ARG2), TermNil);
-  to = CopyTermToArena(entry, FALSE, TRUE, 2, &arena,
+  ARG2 = MkPairTerm(Deref(ARG2), TermNil);
+  to = CopyTermToArena(ARG2, FALSE, TRUE, 2, &arena,
                        min_size PASS_REGS);
   if (to == 0L)
     return FALSE;
