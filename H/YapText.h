c351d6c3b0c2e77893bba062a326ada33caccfdc
vscosta <vscosta@users.noreply.github.com>
2025-07-22 15:22:58 +0100

    update

--- H/YapText.h
+++ H/YapText.h
@@ -45,8 +45,6 @@ Support for text processing:
 
 
 
-
-
 #ifndef Yap_Min
 #define Yap_Min(x, y) (x < y ? x : y)
 
@@ -229,6 +227,18 @@ static inline seq_type_t mod_to_type(int quote, Term mod USES_REGS) {
 }
 
 // the routines
+
+
+inline static Term MkCharTerm(Int c) {
+  CACHE_REGS
+  unsigned char cs[8];
+  if (c==EOF)
+    return TermEof;
+  size_t n = put_xutf8(cs, c);
+  if (n<0) n = 0;
+  cs[n] =  0;
+  return MkAtomTerm(Yap_ULookupAtom(cs));
+}
 
 extern unsigned char *Yap_readText(seq_tv_t *inp USES_REGS);
 extern bool write_Text(unsigned char *inp, seq_tv_t *out USES_REGS);
@@ -622,8 +632,8 @@ static inline Term Yap_UTF8ToDiffListOfCodes(const unsigned char *s,
     // assumes the two code have always the same size;
     utf8proc_int32_t chr;
     s += get_utf8(s, -1, &chr);
-    HR += 2;
     *p = AbsPair(HR - 2);
+    HR += 2;
     p = HR - 1;
     HR[-2] = MkIntTerm(chr);
   }
@@ -634,17 +644,19 @@ static inline Term Yap_UTF8ToDiffListOfCodes(const unsigned char *s,
 
 static inline Term Yap_UTF8ToDiffListOfChars(const unsigned char *s,
 					     Term tail USES_REGS) {
-  seq_tv_t inp,out;
-
-  inp.val.uc0 = s;
-  inp.type = YAP_STRING_CHARS;
-  inp.enc = out.enc = ENC_ISO_UTF8;
-  out.type = YAP_STRING_DIFF | YAP_STRING_ATOMS;
-  out.val.uc = NULL;
-  out.dif = tail;
-  if (!Yap_CVT_Text(&inp, &out PASS_REGS))
-    return 0;
-  return out.val.t;
+  Term p0, *p = &p0;
+  while (*s) {
+    // assumes the two code have always the same size;
+    utf8proc_int32_t chr;
+    s += get_utf8(s, -1, &chr);
+    *p = AbsPair(HR - 2);
+    HR += 2;
+    p = HR - 1;
+    HR[-2] = MkCharTerm(chr);
+  }
+  RESET_VARIABLE(p);
+  Yap_unify(*p, tail);
+  return p0;
 }
 
 static inline Term Yap_WCharsToDiffListOfCodes(const wchar_t *s,
@@ -898,7 +910,7 @@ static inline Term Yap_ListToNumber(Term t0 USES_REGS) {
 
 static inline Term Yap_ListToString(Term t0 USES_REGS) {
   seq_tv_t inp, out;
-  Term TermEmpty = MkAtomTerm(Yap_LookupAtom("")); 
+
     if (t0==TermNil) {
     return TermEmpty;
   } else if (IsPairTerm(t0)) {
@@ -1404,10 +1416,9 @@ static inline Term Yap_ConcatStrings(Term t1, Term t2 USES_REGS) {
 
 static inline Atom Yap_SpliceAtom(Term t1, Atom ats[], size_t cut,
 				  size_t max USES_REGS) {
-  Atom AtomEmpty = Yap_LookupAtom("");
   Atom a3 = AtomOfTerm(t1);
   if(cut == 0){
-   ats[0] = AtomEmpty;
+    ats[0] = AtomEmpty;
     ats[1] = a3;
   } else if( cut == max) {
     ats[0]=a3;
--- os/yapio.h
+++ os/yapio.h
@@ -175,16 +175,6 @@ extern uint64_t WideHashFunction(wchar_t *);
 
 extern void Yap_InitAbsfPreds(void);
 
-inline static Term MkCharTerm(Int c) {
-  CACHE_REGS
-  unsigned char cs[8];
-  if (c==EOF)
-    return TermEof;
-  size_t n = put_xutf8(cs, c);
-  if (n<0) n = 0;
-  cs[n] =  0;
-  return MkAtomTerm(Yap_ULookupAtom(cs));
-}
 
 extern char *GLOBAL_cwd;
 
